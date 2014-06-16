d493ba67ddda79b5b98a1d77d627b22421aa46ba
Nick Bottomley <nhbottomley@gmail.com>
2014-06-16 00:20:41 -0700

    Basic functionality & tests

--- monitor.js
+++ monitor.js
@@ -1,51 +1,96 @@
 var monitor = (function(){
 
-  function extend(target, source){
-    for (var key in source) {
-      if (source.hasOwnProperty(key)) {
+  // helper functions
+  function mixin( target, source ){
+    for ( var key in source ) {
+      if ( source.hasOwnProperty( key ) ) {
         target[key] = source[key];
       }
     }
     return target;
   }
 
-  function isFunction(obj) {
-    return obj && Object.prototype.toString.call(obj) === "[object Function]";
-  };
+  function isFunction( obj ) {
+    return obj && Object.prototype.toString.call( obj ) === "[object Function]";
+  }
 
+  function pluck( collection, key ) {
+    return collection.map(function( obj ){
+      return obj[key];
+    });
+  }
+
+  var slice = Function.prototype.call.bind( [].slice );
+
+
+
+  // methods that get mixed into each monitor.
   var methods = {
-    calledWith: function(){
-
-    }
+    reset: function(){
+      this.lastReturn = null;
+      this.callCount = 0;
+      this.calls = [];
+    },
+    returned: function( obj ){
+      return pluck( this.calls, "returnValue" ).indexOf( obj ) > -1;
+    },
   };
 
-  return function makeMonitor(fn) {
-    
-    if (!isFunction(fn)) {
-      throw new TypeError("Pass a function to monitor()!");
+
+
+  // monitor factory function
+  return function makeMonitor( fn ) {
+
+    var obj, method;
+
+    if ( arguments.length > 1 ) {
+      obj = arguments[0];
+      method = arguments[1];
+      fn = obj[method];
     }
 
+    if ( fn == null ){
+      fn = function(){};
+    }
+    
+    if ( !isFunction( fn ) ) {
+      throw new TypeError( "Pass a function to monitor()!" );
+    }
+
+    //
     var fnMonitor = function(){
-      var returnValue = fn.apply(this, arguments);
-      var self = fnMonitor;
+      var error, returnValue;
 
-      self.calls.push({
-        args: arguments,
-        argsStringified: JSON.stringify(arguments),
+      try {
+        returnValue = fn.apply( this, arguments );
+      } catch ( e ) {
+        error = e;
+      }
+
+      fnMonitor.calls.push({
+        args: slice( arguments ),
+        argsStringified: JSON.stringify( arguments ),
         returnValue: returnValue,
-        nth: fnMonitor.callCount
+        timeStamp: Date.now(),
+        context: this,
+        nth: fnMonitor.callCount,
+        error: null || error
       });
-
-      self.callCount += 1;
+      fnMonitor.callCount += 1;
+      fnMonitor.lastReturn = returnValue;
 
       return returnValue;
     };
 
-    fnMonitor._original = fn;
+    fnMonitor.lastReturn = null;
     fnMonitor.callCount = 0;
     fnMonitor.calls = [];
 
-    extend(fnMonitor, methods);
+    mixin( fnMonitor, methods );
+
+    if ( obj && method ) {
+      obj[method] = fnMonitor;
+    }
 
     return fnMonitor;
   };
--- spec/monitor-spec.js
+++ spec/monitor-spec.js
@@ -1,27 +1,116 @@
-describe("monitor()", function(){
+"use strict";
 
-  "use strict";
+var err = new Error();
 
-  var noop = function(){};
+function add( a, b ){
+  return a + b;
+}
 
-  it("exists", function(){
+function thrower(){
+  throw err;
+}
+
+describe( "monitor()", function(){  
+
+  var noop = function (){};
+  var obj = {
+    method: function(){}
+  };
+
+  it( "exists", function(){
     expect( monitor ).to.be.a( "function" );
   });
 
-  it("takes a function as it's argument", function(){
+  it( "can take a function as it's argument", function(){
     expect(function() {
       monitor( noop );
     }).to.not.throw();
   });
 
-  it("throws an error when passed a non-function")
+  it( "can take an object and a method name as arguments", function(){
+    var m = monitor( obj, "method" );
+    expect( obj.method ).to.equal( m );
+  })
+
+  it( "throws an error when passed a non-function", function(){
     expect( function() {
       monitor( true );
-    }).to.throw(TypeError);
+    }).to.throw( TypeError );
   });
 
-  it("returns a function", function() {
+  it( "returns a function", function() {
     expect( monitor( noop ) ).to.be.a( "function" );
   });
+
+});
+
+
+// instance methods
+describe( "properties", function(){
+  var ctx = {};
+  var m = monitor( add );
+  var t = monitor( thrower );
+  var time = Date.now();
+  m.call( ctx, 10, 20 );
+  t();
+
+  describe( ".calls", function(){
+    it("exists", function(){
+      expect( m.calls ).to.be.an( "array" );
+    });
+  
+    it("stores arguments passed to each call", function(){
+      expect( m.calls.length ).to.equal( 1 );
+      expect( m.calls[0].args[0] ).to.equal( 10 );
+      expect( m.calls[0].args[1] ).to.equal( 20 );
+    });
+
+    it("stores stringified arguments passed to each call", function(){
+      expect( m.calls.length ).to.equal( 1 );
+      expect( m.calls[0].argsStringified ).to.equal( '{"0":10,"1":20}' );
+    });
+
+    it( "stores return values from each call", function(){
+      expect( m.calls[0].returnValue ).to.equal( 30 );
+    });
+
+    it( "stores 'this' values from each call", function(){
+      expect( m.calls[0].context ).to.equal( ctx );
+    });
+
+    it( "stores errors thrown by each call", function(){
+      expect( t.calls[0].error ).to.equal( err );
+    });
+
+    it( "stores the call number of each call", function(){
+      expect( m.calls[0].nth ).to.equal( 0 );
+    });
+
+    // Should look for a better way to do this.
+    it( "stores the time at which each call occurred", function(){
+      expect( m.calls[0].timeStamp ).to.equal( time );
+    });
+
+  });
+
+  describe( ".callCount", function(){
+    it("stores the number of times the function has been called", function(){
+      expect( m.callCount ).to.be.a( "number" );
+      expect( m.callCount ).to.equal( 1 );
+      m( 20, 40 );
+      expect( m.callCount ).to.equal( 2 );
+    });
+
+  });
+
+  describe( ".lastReturn", function(){
+    it("stores the return value of the most recent call", function(){
+      expect( m.lastReturn ).to.equal( 60 );
+      m( 10, 20 );
+      expect( m.lastReturn ).to.equal( 30 );
+    })
+  });
+
+
 
 });
