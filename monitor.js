5b8e32fdf05bba871d9f948d006316026ba7b18e
Nick Bottomley <nhbottomley@gmail.com>
2014-06-16 12:18:37 -0700

    add helper functions

--- monitor.js
+++ monitor.js
@@ -20,35 +20,64 @@ var monitor = (function(){
     });
   }
 
+  function contains( arr, value ) {
+    if ( Array.isArray( value ) ) {
+      return value.every( function( v ){
+        return contains( arr, v );
+      });
+    } else {
+      return arr.indexOf( value ) > -1;
+    }
+  }
+
+  function flatten( arr, out ) {
+    out = out || [];
+    if ( arr.every( Array.isArray ) ) {
+      [].concat.apply( out, arr );
+    } else {
+      arr.forEach( function( value ) {
+        if ( Array.isArray( value ) ) {
+          flatten( value, out );
+        } else {
+          out.push( value );
+        }
+      });
+    }
+    return out;
+  }
+
   var slice = Function.prototype.call.bind( [].slice );
 
 
 
   // methods that get mixed into each monitor.
   var methods = {
-    reset: function(){
+    reset: function() {
       this.lastReturn = null;
       this.callCount = 0;
       this.calls = [];
     },
-    returned: function( obj ){
-      return pluck( this.calls, "returnValue" ).indexOf( obj ) > -1;
+    returned: function( obj ) {
+      return contains( pluck( this.calls, "returnValue" ), obj );
     },
+    calledWith: function( obj ) {
+        return contains( flatten( pluck( this.calls, "args" ) ), obj ); 
+    }
   };
-
-
 
   // monitor factory function
   return function makeMonitor( fn ) {
 
     var obj, method;
 
+    // handle (object, method) call signature
     if ( arguments.length > 1 ) {
       obj = arguments[0];
       method = arguments[1];
       fn = obj[method];
     }
 
+    // handle () call signature
     if ( fn == null ){
       fn = function(){};
     }
@@ -59,19 +88,22 @@ var monitor = (function(){
 
     //
     var fnMonitor = function(){
-      var error, returnValue;
+      var error, returnValue, time;
 
       try {
+        time = window.performance.now();
         returnValue = fn.apply( this, arguments );
+        time = window.performance.now() - time;
       } catch ( e ) {
         error = e;
       }
 
       fnMonitor.calls.push({
         args: slice( arguments ),
-        argsStringified: JSON.stringify( arguments ),
+        argsStringified: JSON.stringify( slice( arguments ) ),
         returnValue: returnValue,
         timeStamp: Date.now(),
+        executionTime: time,
         context: this,
         nth: fnMonitor.callCount,
         error: null || error
--- spec/monitor-spec.js
+++ spec/monitor-spec.js
@@ -1,116 +1,142 @@
-"use strict";
+(function( monitor, describe, expect, it ){
+  "use strict";
 
-var err = new Error();
+  var err = new Error();
 
-function add( a, b ){
-  return a + b;
-}
+  function add( a, b ) {
+    if ( arguments.length > 2 ) {
+      throw err;
+    }
+    return a + b;
+  }
 
-function thrower(){
-  throw err;
-}
+  function isWithinOf( a, b, n ) {
+    return Math.abs( a - b ) <= n;
+  }
 
-describe( "monitor()", function(){  
+  describe( "monitor()", function() {  
 
-  var noop = function (){};
-  var obj = {
-    method: function(){}
-  };
+    var noop = function () {};
+    var obj = {
+      method: function() {}
+    };
 
-  it( "exists", function(){
-    expect( monitor ).to.be.a( "function" );
-  });
-
-  it( "can take a function as it's argument", function(){
-    expect(function() {
-      monitor( noop );
-    }).to.not.throw();
-  });
-
-  it( "can take an object and a method name as arguments", function(){
-    var m = monitor( obj, "method" );
-    expect( obj.method ).to.equal( m );
-  })
-
-  it( "throws an error when passed a non-function", function(){
-    expect( function() {
-      monitor( true );
-    }).to.throw( TypeError );
-  });
-
-  it( "returns a function", function() {
-    expect( monitor( noop ) ).to.be.a( "function" );
-  });
-
-});
-
-
-// instance methods
-describe( "properties", function(){
-  var ctx = {};
-  var m = monitor( add );
-  var t = monitor( thrower );
-  var time = Date.now();
-  m.call( ctx, 10, 20 );
-  t();
-
-  describe( ".calls", function(){
-    it("exists", function(){
-      expect( m.calls ).to.be.an( "array" );
-    });
-  
-    it("stores arguments passed to each call", function(){
-      expect( m.calls.length ).to.equal( 1 );
-      expect( m.calls[0].args[0] ).to.equal( 10 );
-      expect( m.calls[0].args[1] ).to.equal( 20 );
+    it( "exists", function(){
+      expect( monitor ).to.be.a( "function" );
     });
 
-    it("stores stringified arguments passed to each call", function(){
-      expect( m.calls.length ).to.equal( 1 );
-      expect( m.calls[0].argsStringified ).to.equal( '{"0":10,"1":20}' );
+    it( "can take a function as it's argument", function(){
+      expect(function() {
+        monitor( noop );
+      }).to.not.throw();
     });
 
-    it( "stores return values from each call", function(){
-      expect( m.calls[0].returnValue ).to.equal( 30 );
+    it( "can take an object and a method name as arguments", function() {
+      var m = monitor( obj, "method" );
+      expect( obj.method ).to.equal( m );
     });
 
-    it( "stores 'this' values from each call", function(){
-      expect( m.calls[0].context ).to.equal( ctx );
+    it( "throws an error when passed a non-function as it's only argument", function() {
+      expect( function() {
+        monitor( true );
+      }).to.throw( TypeError );
     });
 
-    it( "stores errors thrown by each call", function(){
-      expect( t.calls[0].error ).to.equal( err );
-    });
-
-    it( "stores the call number of each call", function(){
-      expect( m.calls[0].nth ).to.equal( 0 );
-    });
-
-    // Should look for a better way to do this.
-    it( "stores the time at which each call occurred", function(){
-      expect( m.calls[0].timeStamp ).to.equal( time );
+    it( "returns a function", function() {
+      expect( monitor( noop ) ).to.be.a( "function" );
     });
 
   });
 
-  describe( ".callCount", function(){
-    it("stores the number of times the function has been called", function(){
-      expect( m.callCount ).to.be.a( "number" );
-      expect( m.callCount ).to.equal( 1 );
-      m( 20, 40 );
-      expect( m.callCount ).to.equal( 2 );
+
+  // instance methods
+  describe( "properties", function() {
+    var addMonitor;
+    var ctx = {};
+    beforeEach(function(){
+      addMonitor = monitor( add );
+    });
+
+    describe( ".calls", function() {
+      it("exists", function() {
+        expect( addMonitor.calls ).to.be.an( "array" );
+      });
+    
+      it("stores arguments passed to each call", function() {
+        addMonitor( 10, 20 );
+        expect( addMonitor.calls.length ).to.equal( 1 );
+        expect( addMonitor.calls[0].args[0] ).to.equal( 10 );
+        expect( addMonitor.calls[0].args[1] ).to.equal( 20 );
+      });
+
+      it("stores stringified arguments passed to each call", function() {
+        addMonitor( 10, 20 );
+        expect( addMonitor.calls.length ).to.equal( 1 );
+        expect( addMonitor.calls[0].argsStringified ).to.equal( "[10,20]" );
+      });
+
+      it( "stores return values from each call", function() {
+        addMonitor( 10, 20 );
+        expect( addMonitor.calls[0].returnValue ).to.equal( 30 );
+      });
+
+      it( "stores 'this' values from each call", function() {
+        addMonitor.call( ctx, 10, 20 );
+        expect( addMonitor.calls[0].context ).to.equal( ctx );
+      });
+
+      it( "stores the call number of each call", function() {
+        addMonitor( 10, 20 );
+        expect( addMonitor.calls[0].nth ).to.equal( 0 );
+      });
+
+      it( "stores errors thrown by each call", function() {
+        addMonitor( 10, 20, 30 );
+        expect( addMonitor.calls[0].error ).to.equal( err );
+      });
+
+      // Should look for a better way to do this.
+      it( "stores the time at which each call occurred", function() {
+        var time = Date.now();
+        addMonitor( 10, 20 );
+        // this occasionally fails if you check for straight equality.. w/in 1ms is close enough.
+        expect( isWithinOf( addMonitor.calls[0].timeStamp, time, 1 ) ).to.equal( true );
+      });
+
+      it( "stores the execution time of each call", function() {
+        addMonitor( 10, 20 );
+        expect( addMonitor.calls[0].executionTime ).to.be.a( "number" );
+        expect( addMonitor.calls[0].executionTime ).to.be.lessThan( 1 );
+      });
+
+    });
+
+    describe( ".callCount", function() {
+      var addMonitor;
+      var ctx = {};
+      beforeEach(function() {
+        addMonitor = monitor( add );
+      });
+
+      it("stores the number of times the function has been called", function() {
+        addMonitor( 10, 20 )
+        expect( addMonitor.callCount ).to.be.a( "number" );
+        expect( addMonitor.callCount ).to.equal( 1 );
+        addMonitor( 20, 40 );
+        expect( addMonitor.callCount ).to.equal( 2 );
+      });
+
+    });
+
+    describe( ".lastReturn", function(){
+      it("stores the return value of the most recent call", function() {
+        addMonitor( 10, 20 );
+        expect( addMonitor.lastReturn ).to.equal( 30 );
+        addMonitor( 20, 40 );
+        expect( addMonitor.lastReturn ).to.equal( 60 );
+      });
     });
 
   });
 
-  describe( ".lastReturn", function(){
-    it("stores the return value of the most recent call", function(){
-      expect( m.lastReturn ).to.equal( 60 );
-      m( 10, 20 );
-      expect( m.lastReturn ).to.equal( 30 );
-    })
-  });
-
-
-
-});
+})( monitor, describe, expect, it );
