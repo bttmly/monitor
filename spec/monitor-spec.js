8064c84ce3bf3d9d330766dd6f760cbc78c6e8f1
Nick Bottomley <nhbottomley@gmail.com>
2014-06-25 21:54:49 -0700

    Add more tests

--- spec/monitor-spec.js
+++ spec/monitor-spec.js
@@ -1,9 +1,10 @@
 (function( monitor, describe, expect, it ){
   "use strict";
 
-  var err = new Error();
-  var should = chai.should()
+  var should = chai.should();
 
+  var err = new Error();
+  
   function add( a, b ) {
     if ( arguments.length > 2 ) {
       throw err;
@@ -15,6 +16,18 @@
     return Math.abs( a - b ) <= n;
   }
 
+  function personFactory() {
+    return {
+      age: 30,
+      gender: "male",
+      name: "John Doe",
+      job: "BART Operator",
+      get: function( prop ) {
+        return this[prop];
+      }
+    };
+  }
+
   describe( "monitor()", function() {  
 
     var noop = function () {};
@@ -23,23 +36,32 @@
     };
 
     it( "exists", function() {
-      expect( monitor ).to.be.a( "function" );
+      ( monitor ).should.be.a( "function" );
     });
 
     it( "can take a function as it's argument", function() {
-      expect(function() {
+      (function() {
         monitor( noop );
-      }).to.not.throw();
-    });
-
-    it( "can take an object and a method name as arguments", function() {
-      var m = monitor( obj, "method" );
-      expect( obj.method ).to.equal( m );
+      }).should.not.throw();
     });
 
     it( "throws an error when passed a non-function as it's only argument", function() {
+      ( function() {
+        monitor( {} );
+      }).should.throw( TypeError );
+    });
+
+    it( "can take an object and a method name as arguments", function() {
+      var person = personFactory();
+      ( function() {
+        monitor( person, "get" );
+      }).should.not.throw();
+    });
+
+    it( "throws an error when passed an object and a property that isn't a method", function() {
+      var person = personFactory();
       expect( function() {
-        monitor( true );
+        monitor( person, "age" );
       }).to.throw( TypeError );
     });
 
@@ -48,7 +70,6 @@
     });
 
   });
-
 
   // instance methods
   describe( "wrapping a standalone function", function() {
@@ -61,40 +82,40 @@
 
     describe( ".calls", function() {
       it("exists", function() {
-        expect( addMonitor.calls ).to.be.an( "array" );
+        ( addMonitor.calls ).should.be.an( "array" );
       });
     
       it("stores arguments passed to each call", function() {
         addMonitor( 10, 20 );
-        expect( addMonitor.calls.length ).to.equal( 1 );
-        expect( addMonitor.calls[0].args[0] ).to.equal( 10 );
-        expect( addMonitor.calls[0].args[1] ).to.equal( 20 );
+        ( addMonitor.calls.length ).should.equal( 1 );
+        ( addMonitor.calls[0].args[0] ).should.equal( 10 );
+        ( addMonitor.calls[0].args[1] ).should.equal( 20 );
       });
 
       it("stores stringified arguments passed to each call", function() {
         addMonitor( 10, 20 );
-        expect( addMonitor.calls.length ).to.equal( 1 );
-        expect( addMonitor.calls[0].argsStringified ).to.equal( "[10,20]" );
+        ( addMonitor.calls.length ).should.equal( 1 );
+        ( addMonitor.calls[0].argsStringified ).should.equal( "[10,20]" );
       });
 
       it( "stores return values from each call", function() {
         addMonitor( 10, 20 );
-        expect( addMonitor.calls[0].returnValue ).to.equal( 30 );
+        ( addMonitor.calls[0].returnValue ).should.equal( 30 );
       });
 
       it( "stores 'this' values from each call", function() {
         addMonitor.call( ctx, 10, 20 );
-        expect( addMonitor.calls[0].context ).to.equal( ctx );
+        ( addMonitor.calls[0].context ).should.equal( ctx );
       });
 
       it( "stores the call number of each call", function() {
         addMonitor( 10, 20 );
-        expect( addMonitor.calls[0].nth ).to.equal( 0 );
+        ( addMonitor.calls[0].nth ).should.equal( 0 );
       });
 
       it( "stores errors thrown by each call", function() {
         addMonitor( 10, 20, 30 );
-        expect( addMonitor.calls[0].error ).to.equal( err );
+        ( addMonitor.calls[0].error ).should.equal( err );
       });
 
       // Should look for a better way to do this.
@@ -102,13 +123,13 @@
         var time = Date.now();
         addMonitor( 10, 20 );
         // this occasionally fails if you check for straight equality.. w/in 1ms is close enough.
-        expect( isWithinOf( addMonitor.calls[0].timeStamp, time, 1 ) ).to.equal( true );
+        ( isWithinOf( addMonitor.calls[0].timeStamp, time, 1 ) ).should.equal( true );
       });
 
       it( "stores the execution time of each call", function() {
         addMonitor( 10, 20 );
-        expect( addMonitor.calls[0].executionTime ).to.be.a( "number" );
-        expect( addMonitor.calls[0].executionTime ).to.be.lessThan( 1 );
+        ( addMonitor.calls[0].executionTime ).should.be.a( "number" );
+        ( addMonitor.calls[0].executionTime ).should.be.lessThan( 1 );
       });
 
     });
@@ -121,11 +142,11 @@
       });
 
       it("stores the number of times the function has been called", function() {
-        addMonitor( 10, 20 )
-        expect( addMonitor.callCount ).to.be.a( "number" );
-        expect( addMonitor.callCount ).to.equal( 1 );
+        addMonitor( 10, 20 );
+        ( addMonitor.callCount ).should.be.a( "number" );
+        ( addMonitor.callCount ).should.equal( 1 );
         addMonitor( 20, 40 );
-        expect( addMonitor.callCount ).to.equal( 2 );
+        ( addMonitor.callCount ).should.equal( 2 );
       });
 
     });
@@ -133,9 +154,9 @@
     describe( ".lastReturn", function(){
       it("stores the return value of the most recent call", function() {
         addMonitor( 10, 20 );
-        expect( addMonitor.lastReturn ).to.equal( 30 );
+        ( addMonitor.lastReturn ).should.equal( 30 );
         addMonitor( 20, 40 );
-        expect( addMonitor.lastReturn ).to.equal( 60 );
+        ( addMonitor.lastReturn ).should.equal( 60 );
       });
     });
 
@@ -148,22 +169,14 @@
     var monitoredGet;
 
     beforeEach( function() {
-      person = {
-        age: 30,
-        gender: "male",
-        name: "John Doe",
-        job: "BART Operator",
-        get: function( prop ) {
-          return this[prop];
-        }
-      };
+      person = personFactory();
       original = person.get;
       monitoredGet = monitor( person, "get" );
     });
 
     it( "replaces the target method on the object", function() {
-      expect( person.get ).to.equal( monitoredGet );
-      expect( person.get ).to.not.equal( original );
+      ( person.get ).should.equal( monitoredGet );
+      ( person.get ).should.not.equal( original );
     });
 
     it( "is executed in the correct context", function() {
@@ -178,9 +191,15 @@
       ( person.get ).should.equal( original );
     });
 
-
-
-
+    it( "has the expected properties and methods", function() {
+      ( person.get ).should.have.ownProperty( "calls" );
+      ( person.get ).should.have.ownProperty( "callCount" );
+      ( person.get ).should.have.ownProperty( "lastReturn" );
+      ( person.get ).should.have.ownProperty( "reset" );
+      ( person.get ).should.have.ownProperty( "returned" );
+      ( person.get ).should.have.ownProperty( "calledWith" );
+      ( person.get ).should.have.ownProperty( "calledOn" );
+    });
 
   });
 
